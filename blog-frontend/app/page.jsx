fa5e7c5be43f73d33133069f4c3a758a40d171b7
Jakhongir Burkhonov <112249104+Jakhongir222@users.noreply.github.com>
2022-12-26 00:32:01 +0100

    Fixed some stuff

---
+++ blog-frontend/app/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../styles/layout.css";
+
+
+function homepage () {
+
+    return (
+        <div className='welcoming-quote'>
+            <h1>Live your life by a compass, not a clock!</h1>
+        </div>
+    )
+}
+
+export default homepage
