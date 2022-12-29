169a54d2ce00e95642bc606a0792106e03837555
Jakhongir Burkhonov <112249104+Jakhongir222@users.noreply.github.com>
2022-12-29 19:24:41 +0100

    Home page is almost ready

--- blog-frontend/app/page.jsx
+++ blog-frontend/app/page.jsx
@@ -1,5 +1,6 @@
 "use client"
 import "../styles/layout.css";
+import Image from 'next/image'
 import { React, useState } from 'react'
 
 
@@ -9,6 +10,7 @@ function homepage () {
 "Portugal", "Australia", "You have no money to travel", "Nowhere", "Neverland", "we have been trying to reach you about your car's extended warranty", 
 "Ooh, sorry, try next year", "Turkey", "Iceland", "Chile", "Mexico", "Korea", "Nepal", "India", "Thailand"];
     const [randomCity, setRandomCity] =useState('');
+    const [isSpinning, setIsSpinning] = useState(false);
 
     function play() {
         var audio = document.getElementById('b1');
@@ -16,11 +18,11 @@ function homepage () {
       }
  
     const spinTheCompass = () => {
-
         const random = Math.floor(Math.random() * cities.length);
         const newCity = cities[random]
         setRandomCity(newCity);
         play();
+        setIsSpinning(!isSpinning);
     }
 
     return (
@@ -28,7 +30,7 @@ function homepage () {
             <h1>Live your life by a compass, not a clock!</h1>
             <div className="home-page-cart">
                 <p>Where will you be travelling in 2023 ?</p>
-                <button onClick={spinTheCompass}>Click to find out</button>
+                <button onClick={spinTheCompass} className={isSpinning ? 'spin' : ''}>Click me</button>
                 <audio id='b1'><source src="/sound3.wav" type='audio/mpeg'/></audio>
                 <p>{randomCity}</p>
                     <a href='/destinations'>
