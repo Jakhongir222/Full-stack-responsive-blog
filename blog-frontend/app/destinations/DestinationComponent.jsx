4ba1e0bf09e9826f225bd6742234ed503b837c8e
Jakhongir Burkhonov <112249104+Jakhongir222@users.noreply.github.com>
2022-12-29 23:28:02 +0100

    Everything is ready to deploy

--- blog-frontend/app/destinations/DestinationComponent.jsx
+++ blog-frontend/app/destinations/DestinationComponent.jsx
@@ -4,6 +4,10 @@ import "../../styles/destination.css"
 
 function DestinationComponent() {
 
+  const images = {'Uzbekistan': 'https://images.unsplash.com/photo-1591139308596-9b663fa6d0a0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8SXJhbnxlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60',
+    'Sweden': 'https://images.unsplash.com/photo-1509356843151-3e7d96241e11?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8U3dlZGVufGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+    'Japan': 'https://images.unsplash.com/photo-1480796927426-f609979314bd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8SmFwYW58ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'};
+
   const [data, setData] = useState(null)
   const [isLoading, setLoading] = useState(false)
   const baseURL = 'http://localhost:8080/article';
@@ -22,9 +26,15 @@ function DestinationComponent() {
   if (!data) return <p>Page is loading</p>
 
   return (
-    <div>
+    <div className='articles'>
       {data.map((item, index) => (
-        <><p key={index}>{item.articleTitle}</p><p key={item.articleId}>{item.articleText}</p></>
+        <>
+          <div className='article-cart'>
+          <img className='article-image' src={images[item.articleTitle]} width='368' height='250'/>
+            <p className='article-title' key={index}>{item.articleTitle}</p>
+            <p className='article-text' key={item.articleId}>{item.articleText}</p>
+          </div>
+        </>
       ))}
     </div>
   )
